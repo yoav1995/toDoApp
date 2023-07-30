acdb0abf242d4538c5e7834d9d96c2f723dbf514
Yoav Herman <62719732+yoav1995@users.noreply.github.com>
2023-07-30 17:03:39 +0300

    last commit

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -5,16 +5,25 @@ import androidx.appcompat.widget.AppCompatEditText;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.todoapp.Adapters.TaskAdapter;
 import com.example.todoapp.Models.Task;
 import com.example.todoapp.Utilities.DataManager;
 import com.example.todoapp.Utilities.MySp;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textview.MaterialTextView;
 import com.google.gson.Gson;
 
+import java.text.BreakIterator;
 import java.util.ArrayList;
 import java.util.Comparator;
 
@@ -22,7 +31,11 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView lst_tasks;
     private AppCompatEditText task_editor,priority_editor;
     private MaterialButton add_task;
+    private MaterialTextView simple_text;
 
+    private StringRequest stringRequest;
+
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         findViews();
         savedRecycleredView();
+        //sendStringRequest(simple_text,requestQueue,stringRequest);
         // listener for adding tasks
         add_task.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
                 addTasks(task,priority);
             }
         });
-
     }
 
     private void savedRecycleredView() {
@@ -83,8 +96,43 @@ public class MainActivity extends AppCompatActivity {
         task_editor=findViewById(R.id.task_editor);
         priority_editor=findViewById(R.id.priority_editor);
         add_task=findViewById(R.id.add_task);
+        simple_text=findViewById(R.id.simple_text);
+    }
+
+    private void sendStringRequest(MaterialTextView text,RequestQueue queue,StringRequest stringRequest)
+    {
+        queue = Volley.newRequestQueue(this);
+        String url = "https://www.google.com";
+
+// Request a string response from the provided URL.
+       stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        BreakIterator textView;
+                        text.setText("Response is: " + response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                BreakIterator textView;
+                text.setText("That didn't work!");
+            }
+        });
+        queue.add(stringRequest);
     }
 
 
 
+    private void cancelRequestQueue(RequestQueue requestQueue)
+    {
+        requestQueue.cancelAll(this);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        cancelRequestQueue(requestQueue);
+    }
 }
